8b1e84c4315924890a02eab9063e715e215ec736
Trang Lam <ms.tranglam@gmail.com>
2022-03-14 23:35:28 -0700

    completed

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+
+//Part1
+let firstName = "Hermione"
+let lastName = "Granger"
+let birthYear = 1979
+let nationality = "English"
+let boodStatus = " Muggle-born"
+let gender = "Female"
+var height = (5*12)+5
+var weight = 118
+
+//Part2
+var inMovie = true
+var inBeast = true
+var isGumbledoresArmy = true
+var isDeathEater = false
+
+//Part3
+var howImportant = "important"
+var performanceRating = 8
+var reason = "She is an intelligent and kind person"
+
+print(firstName, lastName)
